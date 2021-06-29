c0e84b381227cb2a2317f759d4508316360ab3ca
Rouslan Gaisin <rouslan.gaisin@gmail.com>
2021-06-29 23:53:58 +0300

    Add c2-eks utility

--- setup.py
+++ setup.py
@@ -12,7 +12,11 @@ def get_description():
         return readme.read()
 
 install_requires = [
-    "boto", "lxml", "six"
+    "boto",
+    "lxml",
+    "six",
+    "boto3 @ git+https://github.com/C2Devel/boto3.git@develop",
+    "inflection",
 ]
 # argparse moved to stdlib in python2.7
 if sys.version_info[0] == 2 and sys.version_info[1] <= 6:
@@ -46,6 +50,7 @@ setup(
             "c2-ct = c2client.shell:ct_main",
             "c2-cw = c2client.shell:cw_main",
             "c2-ec2 = c2client.shell:ec2_main",
+            "c2-eks = c2client.shell:eks_main",
             "c2rc-convert = c2client.c2rc_convert:main",
         ]
     },
